05b064451743cf18ac5d7e8b30aef06f4db994e9
titus1333 <161575581+titus1333@users.noreply.github.com>
2024-02-28 00:32:43 -0800

    Add files via upload

---
+++ pelatihan 1 titus.py
@@ -0,0 +1,12 @@
+name = "titus s aflili"
+age = "20" 
+live = "gelanggan cisaat" 
+hobby = "football"
+gender = "male"
+
+print("my name is=",name)
+print("my age=",age)
+print("my live=",live)
+print("my hobby=", hobby)
+print("my gender=",gender)
+
